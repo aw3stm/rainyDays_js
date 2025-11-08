50a6440a79a4ce19dd89451cbf273077aafe1e26
Anna Vestman <Anna.v.westman@gmail.com>
2025-11-08 23:46:19 +0100

    Added product.js
    Worked on styling
    Images
    fetching single product

--- js/index.js
+++ js/index.js
@@ -1,47 +1,46 @@
-"use strict";
-
+const container = document.querySelector("#container");
 const API_URL = "https://v2.api.noroff.dev/rainy-days";
-let allProducts = [];
-const resultsContainer = document.querySelector("#resultsContainer");
 
 async function fetchProducts() {
  try {
   const response = await fetch(API_URL);
-  if (!response.ok) {
-   throw new Error(`HTTP error. Status: ${response.status} `);
-  }
-  const result = await response.json();
-  allProducts = result.data;
+  const data = await response.json();
+  const products = data.data;
 
-  allProducts.forEach((product) => {
+  products.forEach((product) => {
    const card = document.createElement("div");
    const image = document.createElement("img");
    const content = document.createElement("div");
    const title = document.createElement("h2");
    const price = document.createElement("p");
+   const button = document.createElement("button");
+   const anchor = document.createElement("a");
 
    card.className = "card";
    image.className = "card-image";
    content.className = "card-content";
    title.className = "card-title";
    price.className = "card-price";
+   button.className = "cart-btn";
 
    image.src = product.image.url;
    image.alt = product.description;
    title.textContent = product.title;
    price.textContent = product.price;
+   button.textContent = "Add to cart";
+   anchor.href = `product/index.html?id=${product.id}`;
 
    card.appendChild(image);
    card.appendChild(content);
    content.appendChild(title);
    content.appendChild(price);
+   content.appendChild(button);
+   anchor.appendChild(card);
 
-   resultsContainer.appendChild(card);
+   container.appendChild(anchor);
   });
  } catch (error) {
   console.error("Failed to fetch products:", error);
-  resultsContainer.innerHTML =
-   '<p class="error-message">Could not load products. Please try refreshing the page.</p>';
  }
 }
 

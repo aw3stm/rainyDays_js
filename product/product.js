a075d1e1aae677e583d48b420816c9c52d2b3775
Anna Vestman <Anna.v.westman@gmail.com>
2025-11-10 12:03:53 +0100

    Trying out some styling and js

--- product/product.js
+++ product/product.js
@@ -16,46 +16,73 @@ async function fetchProducts() {
   const product = data.data;
 
   const productDiv = document.createElement("div");
+  const textContainer = document.createElement("div");
+  const buttonsContainer = document.createElement("div");
   const image = document.createElement("img");
   const title = document.createElement("h2");
   const price = document.createElement("p");
   const description = document.createElement("p");
+  const sizeOption = document.createElement("p");
   const sizesContainer = document.createElement("div");
   const backButton = document.createElement("a");
+  const addToCartBtn = document.createElement("a");
 
   productDiv.className = "product-details";
+  textContainer.className = "product-text";
+  buttonsContainer.className = "product-buttons";
   image.className = "product-image";
   title.className = "product-title";
   price.className = "product-price";
   description.className = "product-description";
+  sizeOption.className = "size-opt";
   sizesContainer.className = "product-sizes-container";
   backButton.className = "back-button";
+  addToCartBtn.className = "cart-btn";
 
   image.src = product.image.url;
-  image.description = product.image.description;
+  image.alt = product.image.alt;
   title.textContent = product.title;
   price.textContent = `$${product.price}`;
   description.textContent = product.description;
+  sizeOption.textContent = "Select size";
   backButton.textContent = "Back to Products";
   backButton.href = "../index.html";
+  addToCartBtn.textContent = "Add to cart";
+  addToCartBtn.href = "../checkout/index.html";
 
-  product.sizes.forEach(size => {
-    const button = document.createElement("button");
-    button.textContent = size;
-    button.className = "size-button";
-    sizesContainer.appendChild(button)
-    
+  let selectedSize = "";
+  let selectedButton = "";
+
+  product.sizes.forEach((size) => {
+   const button = document.createElement("button");
+   button.textContent = size;
+   button.className = "size-button";
+
+   button.addEventListener("click", () => {
+    if (selectedButton) {
+     selectedButton.classList.remove("selected");
+    }
+    selectedSize = size;
+    selectedButton = button;
+    button.classList.add("selected");
+
+    console.log("Selected size:", selectedSize);
+   });
+   sizesContainer.appendChild(button);
   });
 
-  productDiv.appendChild(image);
-  productDiv.appendChild(title);
-  productDiv.appendChild(price);
-  productDiv.appendChild(description);
-  productDiv.appendChild(sizesContainer);
-  productDiv.appendChild(backButton);
+  textContainer.appendChild(title);
+  textContainer.appendChild(price);
+  textContainer.appendChild(description);
+  textContainer.appendChild(sizeOption);
+  textContainer.appendChild(sizesContainer);
+  buttonsContainer.appendChild(backButton);
+  buttonsContainer.appendChild(addToCartBtn);
 
+  productDiv.appendChild(image);
+  productDiv.appendChild(textContainer);
+  textContainer.appendChild(buttonsContainer);
   container.appendChild(productDiv);
- 
  } catch (error) {
   console.error("Failed to fetch product", error);
   container.textContent = "Failed to load product, please try again!";
@@ -63,4 +90,3 @@ async function fetchProducts() {
 }
 
 fetchProducts();
-
